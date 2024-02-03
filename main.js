23c8836f2477c231581a0353b0f16a16998ad51a
Ebraheem <ebraheem20012020@gmail.com>
2024-02-03 20:35:56 +0200

    typo

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 
 function redirect(){
     e = document.getElementById("nid")
-    if (e.value == 3011151603298)
+    if (e.value == 30111151603298)
         window.location.replace("./result.html")
     else window.location.replace("./not_found.html")
 }
